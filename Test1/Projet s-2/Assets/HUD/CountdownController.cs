e64cc837d930b7cfbf0f94a57b69ad864de650f2
Stefano <stefano.mancinelli@epita.fr>
2020-03-11 00:54:38 +0100

    Pussy Slayer

---
+++ Test1/Projet s-2/Assets/HUD/CountdownController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownController : MonoBehaviour
+{
+  public int countdownTime;
+  public Text countdownDisplay;
+
+  private void Start()
+  {
+    StartCoroutine(CountdownToStart());
+  }
+
+
+  IEnumerator CountdownToStart()
+  {
+    while (countdownTime > 0)
+    {
+      countdownDisplay.text = countdownTime.ToString();
+      
+      yield return new WaitForSeconds(1f);
+
+      countdownTime--;
+    }
+
+    countdownDisplay.text = "GAMER OVER!";
+    
+    yield return new WaitForSeconds(1F);
+    
+    countdownDisplay.gameObject.SetActive(false);
+  }
+  
+}
---
+++ Test1/Projet s-2/Assets/HUD/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private Stat health;
+
+    private void Awake()
+    {
+        health.Initialize();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            health.CurrentVal -= 10;
+        }
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            health.CurrentVal += 10;
+        }
+    }
+}
